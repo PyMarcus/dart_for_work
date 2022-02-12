d229d5a4c79e3a7a1c46ead2a7f39075c926ce8f
PyMarcus <marcus-v@outlook.com>
2022-02-12 18:43:05 -0300

    adicionado atualizações

---
+++ dart/13map.dart
@@ -0,0 +1,14 @@
+main(){
+  const maps = {
+    "valor1": 1,
+    "valor2": 'dois',
+  };
+  print(maps);
+  print(maps['valor1']);
+
+
+  Map<int, String> mapa_com_tipo = {
+    1: "olá",
+    2: "Oi"
+  };
+}
